0d187ee36b734579eacce87266b5d291da23b75e
shubham <vishvakarma723@gmail.com>
2025-12-20 13:16:39 +0530

    Add sum.java

---
+++ sum.java
@@ -0,0 +1,9 @@
+class addition {
+    public static void main(string[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        system.out.println("sum = " + sum);
+    }
+}
